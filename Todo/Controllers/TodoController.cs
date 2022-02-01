be4c702218ba93d46ee077812b65b688247a40fe
akmaL199601 <akmal199601@mail.ru>
2022-02-01 09:27:29 +0500

    Add Context

---
+++ Todo/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestRun()
+        {
+            return Ok("succes");
+        }
+        
+    }
+}
--- Todo/Startup.cs
+++ Todo/Startup.cs
@@ -14,23 +14,18 @@ using Microsoft.OpenApi.Models;
 
 namespace Todo
 {
-    public class Startup
+    public class Startup    
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Todo", Version = "v1"}); });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
